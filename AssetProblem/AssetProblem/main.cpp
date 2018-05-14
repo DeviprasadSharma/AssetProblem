4dd32632674771db76827a195adf54f615c4784d
Rex Low <qiweilow950823@gmail.com>
2018-05-15 01:35:57 +0800

    init project

---
+++ AssetProblem/AssetProblem/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AssetProblem
+//
+//  Created by Rex Low on 15/05/2018.
+//  Copyright © 2018 Rex Low. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
