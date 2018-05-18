0f32ff9c553fbb42cd3fff7327ef900145e74205
Rex Low <qiweilow950823@gmail.com>
2018-05-19 00:40:19 +0800

    shortened code

--- AssetProblem/AssetProblem/main.cpp
+++ AssetProblem/AssetProblem/main.cpp
@@ -10,6 +10,7 @@ using namespace std;
 #define BUDGET 40000
 #define MAX_PRICE 63550
 #define MIN_PRICE 0
+#define CROSSOVER_PROB 1.0
 
 //constants
 const int ITEMS[GENE_SIZE] = {
@@ -38,18 +39,40 @@ void printChromosome() {
     }
 }
 
+void copyParent() {
+    for (int i=0; i<PARENT_SIZE; i++) {
+        for (int j=0; j<GENE_SIZE; j++) children[i][j] = parent[i][j];
+    }
+}
+
+void printParent() {
+    for (int i=0; i<PARENT_SIZE; i++) {
+        cout << "Parent   " << i+1 << " : ";
+        for (int j=0; j<GENE_SIZE; j++) cout << parent[i][j] << " ";
+        cout << endl;
+    }
+}
+
+void printChildren() {
+    for (int i=0; i<PARENT_SIZE; i++) {
+        cout << "Children " << i+1 << " : ";
+        for (int j=0; j<GENE_SIZE; j++) cout << children[i][j] << " ";
+        cout << endl;
+    }
+}
+
 void initializeChromosome() {
     for (int i=0; i<POP_SIZE; i++) {
         for (int j=0; j<GENE_SIZE; j++) chromosome[i][j] = rand() % 2;
     }
 }
 
-// goal 1 - maximize number of items (number of 1s in the gene pool)
-// goal 2 - total price should be as closed as budget
+// goal 1 - total price should be as closed as budget
+// goal 2 - maximize number of items (number of 1s in the gene pool)
 // goal 3 - prioritize certain items
 // currently goals are not associated with weight (importance), review later
 void evaluateChromosome() {
-    cout << "********************\t Evaluate Chromosome \t********************\n";
+    cout << "********************\t Evaluate Chromosome Begin \t********************\n";
     for (int i = 0; i < POP_SIZE; i++) {
         int accPrice = 0;
         double  f1 = 0.0, f2 = 0.0, f3 = 0.0, nItems = 0.0, totalPriority = 0;
@@ -70,11 +93,11 @@ void evaluateChromosome() {
         
         cout << "Chromo " << i+1 << "\tPrice: RM " << accPrice << "\tItems: " << nItems << " \tPriority: " << totalPriority <<  " \tFitness: " << fitness[i] << endl;
     }
-    cout << endl;
+    cout << "********************\t Evaluate Chromosome End \t********************\n\n\n";
 }
 
 void parentSelection() {
-    cout << "********************\t Parent Selection \t********************\n";
+    cout << "********************\t Parent Selection Begin \t********************";
     int player1, player2, bestPlayer;
     for (int i=0; i<PARENT_SIZE; i++) {
         
@@ -88,19 +111,47 @@ void parentSelection() {
             player2 = rand() % POP_SIZE;
         }
         
-        cout << "\nPARENT " << i + 1 << " :\n";
+        cout << "\nParent   " << i + 1 << " :\n";
         cout << "\tPlayer 1 : Chromo[" << player1 + 1 << "]\tFitness : " << fitness[player1] << " \n";
         cout << "\tPlayer 2 : Chromo[" << player2 + 1 << "]\tFitness : " << fitness[player2] << " \n";
         
         // find out who is the best player by comparing fitness
         if (fitness[player1] > fitness[player2]) bestPlayer = player1; else bestPlayer = player2;
-        cout << "\tWinning player ---> Chromo[" << bestPlayer + 1 << "]\n";
+        cout << "\n\tWinning player ---> Chromo[" << bestPlayer + 1 << "]\n";
         
         // allocate gene of best player to the parent array
         for (int j=0; j<POP_SIZE; j++) {
             for (int k=0; k<GENE_SIZE; k++) parent[i][k] = chromosome[bestPlayer][k];
         }
     }
+    
+    cout << endl;
+    printParent();
+    cout << "********************\t Parent Selection Begin \t********************\n\n\n";
+}
+
+void crossover() {
+    cout << "********************\t Crossover Parent Begin \t********************\n";
+    
+    double cProb = ( rand() % 11 ) / 10.0;
+    cout << "Crossover Probability : " << cProb << endl;
+    
+    copyParent();
+    
+    if (cProb < CROSSOVER_PROB) {
+        int cPoint = rand() % GENE_SIZE;
+        cout << "Crossover did happen at point " << cPoint+1 << endl;
+        
+        for (int i=cPoint+1; i<GENE_SIZE; i++) {
+            children[0][i] = parent[1][i];
+            children[1][i] = parent[0][i];
+        }
+    } else {
+        cout << "Crossover did not happen" << endl;
+    }
+    
+    printChildren();
+    cout << "********************\t Crossover Parent End \t********************\n\n\n";
 }
 
 int main(int argc, const char * argv[]) {
@@ -108,5 +159,6 @@ int main(int argc, const char * argv[]) {
     initializeChromosome();
     evaluateChromosome();
     parentSelection();
+    crossover();
     return 0;
 }
